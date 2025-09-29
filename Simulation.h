7fbcef500f2dd066cabfb69cff294e74d70b5f63
My Little Jellyfish <unsigned.angler.19937@proton.me>
2025-09-29 14:02:30 +0300

    chore(simulation): minor

--- Simulation.h
+++ Simulation.h
@@ -3,21 +3,22 @@
 
 #include <map>
 #include <memory>
+
 #include "Generator.h"
 
 class Simulation {
-private:
-    std::shared_ptr<Generator> generator;
-    int totalGenerations;
-    std::map<int, int> results;
+ private:
+  std::shared_ptr<Generator> generator;
+  int totalGenerations;
+  std::map<int, int> results;
 
-public:
-    Simulation(std::shared_ptr<Generator> gen, int total);
+ public:
+  Simulation(std::shared_ptr<Generator> gen, int total);
 
-    void operator()();
+  void operator()();
 
-    const std::map<int, int>& getResults() const;
-    int getTotalGenerations() const;
+  const std::map<int, int>& getResults() const;
+  int getTotalGenerations() const;
 };
 
-#endif //SIMULATION_H
+#endif  // SIMULATION_H
